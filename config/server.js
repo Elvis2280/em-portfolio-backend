ba020db90daac5b6ec2f2ea827f012e2cf3d8bcb
Elvis Miranda <elvismiranda924g@gmail.com>
2021-08-02 18:46:40 -0500

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a7790eec675b3daaa07f3aaefb28507e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '570b232f-8809-48a3-89c0-d51abc242580'
+};
